fcda22a33fa9a75d6a4ab6180671fd93bac3ac6e
gk2Q34X3 <emirceliknet@gmail.com>
2023-01-29 23:56:19 +0300

    Adds the Fontawsome Vue package

---
+++ resources/js/Sources/icons.js
@@ -0,0 +1,9 @@
+//Core import function
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+//Icons Libs
+import {  } from "@fortawesome/free-solid-svg-icons";
+import {  } from "@fortawesome/free-regular-svg-icons";
+import {  } from "@fortawesome/free-brands-svg-icons";
+
+library.add();
