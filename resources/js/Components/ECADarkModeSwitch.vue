45419380e27aa6a1b70d3ff046ac6f8a337fbead
gk2Q34X3 <emirceliknet@gmail.com>
2023-01-31 13:20:22 +0300

    Adds Darkmode switcher Component

---
+++ resources/js/Components/ECADarkModeSwitch.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useDark, useToggle } from '@vueuse/core'
+
+
+const isDark = useDark({
+    selector: 'html',
+    attribute: 'class',
+    valueDark: 'dark',
+    valueLight: 'light',
+})
+const toggleDark = useToggle(isDark)
+
+</script>
+
+<template>
+    <font-awesome-icon size="lg" @click="toggleDark()" :icon="'fa-solid' + ' ' + (isDark ? 'fa-sun' : 'fa-moon')" class="cursor-pointer hover:scale-110 active:scale-90 duration-300" :class="isDark ? 'text-amber-500' : 'text-slate-700'"/>
+</template>
